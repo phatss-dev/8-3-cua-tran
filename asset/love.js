3efcf2360b1098980b43943d8cda01f330093ad9
phatss-dev <phunghuuphat3@gmail.com>
2025-05-21 23:51:00 +0700

    Update love.js

--- asset/love.js
+++ asset/love.js
@@ -1,105 +1,84 @@
-﻿(function(window) {
+
+function LoveTree(canvas, imageList, callback) {
+  const ctx = canvas.getContext("2d");
+  const width = canvas.width;
+  const height = canvas.height;
 
   function random(min, max) {
-    return min + Math.floor(Math.random() * (max - min + 1));
+    return min + Math.random() * (max - min);
   }
 
-  function Point(x, y) {
-    this.x = x || 0;
-    this.y = y || 0;
+  function drawBloom(x, y, image) {
+    const img = new Image();
+    img.onload = function () {
+      ctx.save();
+      ctx.translate(x, y);
+      ctx.drawImage(img, -15, -15, 30, 30);
+      ctx.restore();
+    };
+    img.src = image;
   }
 
-  Point.prototype = {
-    add: function(o) {
-      return new Point(this.x + o.x, this.y + o.y);
-    },
-    sub: function(o) {
-      return new Point(this.x - o.x, this.y - o.y);
-    },
-    div: function(n) {
-      return new Point(this.x / n, this.y / n);
+  this.plant = function (count) {
+    for (let i = 0; i < count; i++) {
+      const x = random(100, width - 100);
+      const y = random(100, height - 100);
+      const imgIndex = Math.floor(Math.random() * imageList.length);
+      const imgSrc = imageList[imgIndex];
+      drawBloom(x, y, imgSrc);
     }
-  }
+    if (typeof callback === "function") callback();
+  };
 
-  function Bloom(ctx, point, img) {
-    this.ctx = ctx;
-    this.point = point;
-    this.img = img;
-    this.alpha = 1;
-    this.angle = random(0, 360) * Math.PI / 180;
-    this.scale = 0.1;
-  }
+  const fallingHearts = [];
 
-  Bloom.prototype = {
-    draw: function() {
-      if (!this.img.complete) return;
-
-      this.ctx.save();
-      this.ctx.globalAlpha = this.alpha;
-      this.ctx.translate(this.point.x, this.point.y);
-      this.ctx.rotate(this.angle);
-      this.ctx.scale(this.scale, this.scale);
-      this.ctx.drawImage(this.img, -this.img.width / 2, -this.img.height / 2);
-      this.ctx.restore();
-
-      this.scale += 0.02;
-      if (this.scale < 1.2) {
-        requestAnimationFrame(this.draw.bind(this));
-      }
-    }
-  }
-
-  function LoveTree(canvas, callback) {
-    this.canvas = canvas;
-    this.ctx = canvas.getContext('2d');
-    this.images = [];
-
-    const imageList = [
-      'flower/flower1.png',
-      'flower/flower2.png',
-      'flower/flower3.png',
-      'flower/flower4.png',
-      'flower/flower5.png',
-      'flower/flower6.png',
-      'flower/flower7.png',
-      'flower/flower8.png',
-      'flower/flower9.png',
-      'flower/flower10.png',
-      'flower/flower11.png'     
-    ];
-
-    this.loadImages(imageList, callback);
-  }
-
-  LoveTree.prototype = {
-    loadImages: function(urls, callback) {
-      let loaded = 0;
-      const self = this;
-
-      urls.forEach(function(url, i) {
-        const img = new Image();
-        img.src = url;
-        img.onload = function () {
-          loaded++;
-          self.images[i] = img;
-          if (loaded === urls.length && typeof callback === 'function') {
-            callback();
-          }
-        };
+  this.startFalling = function () {
+    for (let i = 0; i < 25; i++) {
+      const img = new Image();
+      img.src = imageList[Math.floor(Math.random() * imageList.length)];
+      fallingHearts.push({
+        x: random(0, width),
+        y: random(-200, -20),
+        speedY: random(1, 2),
+        angle: random(0, Math.PI * 2),
+        angleSpeed: random(0.01, 0.03),
+        amplitude: random(10, 20),
+        img: img,
+        opacity: 1,
+        fading: false
       });
-    },
-
-    plant: function(n) {
-      for (let i = 0; i < n; i++) {
-        const x = random(100, this.canvas.width - 100);
-        const y = random(100, this.canvas.height - 100);
-        const img = this.images[random(0, this.images.length - 1)];
-        const bloom = new Bloom(this.ctx, new Point(x, y), img);
-        bloom.draw();
-      }
     }
-  }
 
-  window.LoveTree = LoveTree;
+    function animate() {
+      ctx.clearRect(0, 0, width, height);
 
-})(window);
+      for (let i = 0; i < fallingHearts.length; i++) {
+        const h = fallingHearts[i];
+        h.y += h.speedY;
+        h.angle += h.angleSpeed;
+        const offsetX = Math.sin(h.angle) * h.amplitude;
+
+        if (h.y > height - 30) h.fading = true;
+        if (h.fading) h.opacity -= 0.02;
+
+        if (h.opacity <= 0) {
+          // Reset láº¡i hoa
+          h.y = random(-200, -20);
+          h.x = random(0, width);
+          h.opacity = 1;
+          h.fading = false;
+        }
+
+        ctx.save();
+        ctx.globalAlpha = h.opacity;
+        ctx.translate(h.x + offsetX, h.y);
+        ctx.drawImage(h.img, -15, -15, 30, 30);
+        ctx.restore();
+      }
+
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+  };
+}
